613e2a835d20422c2f5f5db07ea59914461266b5
JoeMariner <jo3keh@gmail.com>
2017-02-13 09:07:03 -0500

    initial commit

---
+++ MVCReviewSiteProject/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSiteProject.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string System { get; set; }
+        public virtual ICollection<Game> Games { get; set; }
+    }
+}
---
+++ MVCReviewSiteProject/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSiteProject.Models
+{
+    public class Game
+    {
+        [Key]
+        public int ID { get; set; }
+        public string NameOfGame { get; set; }
+        public string ReviewedBy { get; set; }
+        public int YearPublished { get; set; }
+        public string Review { get; set; }
+        public int Rating { get; set; }
+
+        [ForeignKey("Category")]
+        public int categoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
